e64374151f4398aeecf9f1336f3d457291986d5f
Brett Byrd <byrdb2@yahoo.com>
2023-03-30 19:53:23 -0500

    complete

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const len = word.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+ Loop through to check the first character to the last character of the string and then move next
+Compare characters and return false if they do not match 
 */
 
 /*
   Add written explanation of your solution here
-*/
+  "The function compares the characters at the beginning and end of the string. If the characters do not match, the function returns false.
+   "If all the characters match, the function returns true"
+   */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
